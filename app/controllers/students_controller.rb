252db943762dbcd24fe8c8946b20227724b89c35
Jacqueline Lee <anginblu@gmail.com>
2018-01-22 22:10:43 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
     set_student
     @student.active = true
     @student.save
-    redirect "students/#{@student.id}"
+    redirect "/students/#{@student.id}"
   end
 
   private
