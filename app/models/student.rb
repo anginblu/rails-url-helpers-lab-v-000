7fbff705cdc040a94f06bcd60143d706b07fffa1
Jacqueline Lee <anginblu@gmail.com>
2018-01-22 21:57:11 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,17 +3,4 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-
-  # def active=(value=false)
-  #   @active = value
-  # end
-  #
-  # def active
-  #   if @active == nil
-  #     false
-  #   else
-  #     @active
-  #   end
-  # end
-
 end
