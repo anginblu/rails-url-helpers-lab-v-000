5a2ffb6fd085af3495675cc157d7b280458f1f03
Jacqueline Lee <anginblu@gmail.com>
2018-01-22 21:39:51 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 
-  def active
-    default value = false
+  def initialize
+    self.active = false
   end
 end
